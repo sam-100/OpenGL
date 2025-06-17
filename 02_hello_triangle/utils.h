0c8f9f82e6a0673e7339ba21306817dc9e92614a
Samarth Tandale <tandalesamarth@gmail.com>
2025-06-17 15:04:56 +0530

    Added few comments

--- 02_hello_triangle/utils.h
+++ 02_hello_triangle/utils.h
@@ -7,7 +7,10 @@ void error(const char *msg, int error_code);
 
 std::string load_file(const char *file_name);
 
+// loads and compiles the shader from given file and returns its openGL id
 GLuint compile_shader(const char *file_name, GLenum shader_t);
+
+// links vertex and fragment shaders provided to create a shader program and returns the program id
 GLuint link_shaders(GLuint vs, GLuint fs);
 
 #endif
